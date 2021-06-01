84381aab74d4574f8425b15d351b2f6f2f0005d6
duongdam <duong.dam@classfunc.com>
2021-06-01 19:55:04 +0700

    init app

---
+++ lib/main.dart
@@ -0,0 +1,190 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:syncfusion_flutter_calendar/calendar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My calendar',
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''), // English, no country code
+        const Locale('vi'), // Spanish, no country code
+      ],
+      locale: Locale('vi'),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'My calendar Flutter app'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Right to Left'),
+      ),
+      body: SafeArea(
+        child: Directionality(
+          textDirection: TextDirection.ltr,
+          child: SfCalendar(
+            view: CalendarView.week,
+            allowedViews: [
+              CalendarView.day,
+              CalendarView.week,
+              CalendarView.workWeek,
+              CalendarView.month,
+              CalendarView.schedule,
+              CalendarView.timelineDay,
+              CalendarView.timelineWeek,
+              CalendarView.timelineWorkWeek,
+              CalendarView.timelineMonth
+            ],
+            firstDayOfWeek: 1,
+            showDatePickerButton: true,
+            showCurrentTimeIndicator: true,
+            dataSource: MeetingDataSource(_getDataSource()),
+            // by default the month appointment display mode set as Indicator, we can
+            // change the display mode as appointment using the appointment display
+            // mode property
+            monthViewSettings: const MonthViewSettings(
+                appointmentDisplayMode:
+                    MonthAppointmentDisplayMode.appointment),
+            // timeSlotViewSettings: const TimeSlotViewSettings(timeInterval: Duration(hours: 1),timeIntervalHeight: 50,),
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Meeting> _getDataSource() {
+    final List<Meeting> meetings = <Meeting>[];
+    final DateTime today = DateTime.now();
+    final DateTime startTime =
+        DateTime(today.year, today.month, today.day, 19, 0, 0);
+    final DateTime endTime = startTime.add(const Duration(hours: 2));
+    meetings.add(Meeting('TEST', startTime, endTime, const Color(0xFFE80F0F),
+        false, 'FREQ=DAILY;INTERVAL=2;COUNT=20'));
+    meetings.add(Meeting(
+        'TEST2',
+        startTime.add(const Duration(hours: 2)),
+        endTime.add(const Duration(hours: 2)),
+        const Color(0xFF4A06E8),
+        false,
+        'FREQ=DAILY;INTERVAL=2;COUNT=20'));
+    meetings.add(Meeting(
+        'TEST3',
+        startTime.add(const Duration(days: 1)),
+        endTime.add(const Duration(days: 1)),
+        const Color(0xFFEFBD06),
+        false,
+        'FREQ=DAILY;INTERVAL=2;COUNT=20'));
+    meetings.add(Meeting(
+        'TEST4',
+        startTime.add(const Duration(hours: 6)),
+        endTime.add(const Duration(hours: 8)),
+        const Color(0xFF06A4E2),
+        false,
+        'FREQ=DAILY;INTERVAL=2;COUNT=20'));
+    print(meetings[0].from);
+    return meetings;
+  }
+}
+
+/// An object to set the appointment collection data source to calendar, which
+/// used to map the custom appointment data to the calendar appointment, and
+/// allows to add, remove or reset the appointment collection.
+class MeetingDataSource extends CalendarDataSource {
+  /// Creates a meeting data source, which used to set the appointment
+  /// collection to the calendar
+  MeetingDataSource(List<Meeting> source) {
+    appointments = source;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    return _getMeetingData(index).from;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    return _getMeetingData(index).to;
+  }
+
+  @override
+  String getSubject(int index) {
+    return _getMeetingData(index).eventName;
+  }
+
+  @override
+  String getRecurrenceRule(int index) {
+    return _getMeetingData(index).recurrenceRule;
+  }
+
+  @override
+  Color getColor(int index) {
+    return _getMeetingData(index).background;
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return _getMeetingData(index).isAllDay;
+  }
+
+  Meeting _getMeetingData(int index) {
+    final dynamic meeting = appointments![index];
+    late final Meeting meetingData;
+    if (meeting is Meeting) {
+      meetingData = meeting;
+    }
+
+    return meetingData;
+  }
+}
+
+/// Custom business object class which contains properties to hold the detailed
+/// information about the event data which will be rendered in calendar.
+class Meeting {
+  /// Creates a meeting class with required details.
+  Meeting(this.eventName, this.from, this.to, this.background, this.isAllDay,
+      this.recurrenceRule);
+
+  /// Event name which is equivalent to subject property of [Appointment].
+  String eventName;
+
+  /// From which is equivalent to start time property of [Appointment].
+  DateTime from;
+
+  /// To which is equivalent to end time property of [Appointment].
+  DateTime to;
+
+  /// Background which is equivalent to color property of [Appointment].
+  Color background;
+
+  /// IsAllDay which is equivalent to isAllDay property of [Appointment].
+  bool isAllDay;
+
+  /// recurrence rule
+  String recurrenceRule;
+}
